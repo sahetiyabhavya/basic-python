397d9bff70177ea50fc7c6932498baa196bfd8e2
bhavya sahetiya <sahetiyab@gmail.com>
2026-06-25 16:42:40 +0530

    add this

--- function.py
+++ function.py
@@ -1,28 +1,28 @@
-# #  1
-# a =  float(input("enter a number: "))
-# b = float(input("enter a number: "))
+#  1
+a =  float(input("enter a number: "))
+b = float(input("enter a number: "))
 
 
-# def add(a,b):
-#     c= a+b
-#     print(c)
+def add(a,b):
+    c= a+b
+    print(c)
 
-# add(a,b)
-
-
+add(a,b)
 
 
 
-# # 2
-# num = int(input("enter a numbeer : "))
 
-# def add():
-#     if num>25:
-#         print("Its Bigger than 25 :)")
-#     else:
-#         print("Its Smaller than 25 :( ")
 
-# add()
+# 2
+num = int(input("enter a numbeer : "))
+
+def add():
+    if num>25:
+        print("Its Bigger than 25 :)")
+    else:
+        print("Its Smaller than 25 :( ")
+
+add()
 
 
 
